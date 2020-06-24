123203222793ca89859f21682388664cacfc8a76
Hieu Vu <hvu@amperecomputing.com>
2020-06-24 12:11:47 +0700

    Add infer() with forward sampling in BayesNet
    Signed-off-by: Hieu Vu <hvu@amperecomputing.com>

--- bayesnet.py
+++ bayesnet.py
@@ -21,22 +21,50 @@ class BayesNode(VertexNode):
 class BayesNet(DiGraph):
     def __init__(self):
         super().__init__()
-        self.sample_arr = []  # will be convert to numpy array later
+        self.sample_arr = np.array([0])  # will be convert to numpy array later
         self.topo_dict = {}  # Updated in sample()
 
     def add(self, vertex, parent_lst, val_dict, val_table):
         self.node_list.append(BayesNode(vertex, parent_lst, val_dict, val_table))
 
-    def check(self):
+    def infer(self, var_infer_dict, var_proof_dict):
         pass
 
-    def infer(self, var_infer_dict, var_proof_dict):
-        self.var_infer_dict = var_infer_dict
-        self.var_proof_dict = var_proof_dict
-        self.check()
+    def check(self, var_dict):
+        pass
+
+    def infer_forward(self, var_infer_dict, var_proof_dict, sample_num=10**6):
+        # G = A; I = Cao, D = Kho
+        # var_infer_dict = {"G": "A"}
+        # var_proof_dict = {"I": "Cao", "D": "Kho"}
+        self.check(var_infer_dict)
+        self.sample(sample_num)
+
+        proof_mask = []
+        proof_compare = []
+        for vertex, val in var_proof_dict.items():
+            proof_mask.append(self.topo_dict[vertex])
+            node = self.get_vertex_node(vertex)
+            proof_compare.append(node.val_dict[val])
+
+        infer_mask = []
+        infer_compare = []
+        for vertex, val in var_infer_dict.items():
+            infer_mask.append(self.topo_dict[vertex])
+            node = self.get_vertex_node(vertex)
+            infer_compare.append(node.val_dict[val])
+
+        if proof_mask:
+            proof = np.where((self.sample_arr[:, proof_mask] == proof_compare).all(axis=1))
+            infer = np.where((self.sample_arr[proof[0][:, None], infer_mask] == infer_compare).all(axis=1))
+            return infer[0].shape[0] / proof[0].shape[0]
+        else:
+            infer = np.where((self.sample_arr[:, infer_mask] == infer_compare).all(axis=1))
+            return infer[0].shape[0] / self.sample_arr.shape[0]
 
     def sample(self, sample_num):
         np.random.seed(0)
+        sample_lst = []
 
         sorter = TopoSorter(self)
         topo_lst = sorter.sort()
@@ -46,9 +74,9 @@ class BayesNet(DiGraph):
             sample_val = []
             for vertex in topo_lst:
                 sample_val.append(self.roll(vertex, sample_val))
-            self.sample_arr.extend(sample_val)
+            sample_lst.extend(sample_val)
 
-        self.sample_arr = np.array(self.sample_arr).reshape(sample_num, -1)
+        self.sample_arr = np.array(sample_lst).reshape(sample_num, -1)
 
     def roll(self, vertex, prev_vertex_val):
         node = self.get_vertex_node(vertex)
