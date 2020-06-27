16327be03f352c3815e58b922ad51b7362199a19
Vo Tien Dat <datvt4@vng.com.vn>
2020-06-27 20:39:47 +0700

    add option method infer

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def parse_infer(test_file):
 def read_command():
     full_cmd_arguments = sys.argv
     argument_list = full_cmd_arguments[1:]
-    long_options = ["model=", "test="]
+    long_options = ["model=", "test=", "method="]
     file_name = ""
     file_test = ""
+    method = ""
 
     try:
         arguments, values = getopt.getopt(argument_list, "", long_options)
@@ -36,29 +37,41 @@ def read_command():
         if current_argument == "--test":
             file_test = current_value
 
-    return file_name, file_test
+        if current_argument == "--method":
+            method = current_value
+
+    return file_name, file_test, method
 
 
 def main():
-    input_model = read_command()
+    #input_model = read_command()
     input_model = ['model.txt', 'test.txt']
-    if len(input_model) == 2:
+    if len(input_model) >= 2:
         graph_in = parse_graph(input_model[0])
         for i in graph_in.node_dict.values():
             print(i.vertex, " parent_list = ", i.parent_lst, " value_list = ", i.val_dict, " table = ", i.distribution)
 
         infer_in = parse_infer(input_model[1])
+        method = "forward"
+        if len(input_model) == 3 and input_model[2] != "":
+            method = input_model[2]
+
         output_file = open('output.txt', 'w')
         for i in infer_in.infer_list:
+            output_value = ""
+            print("method = ", method)
             print("infer_dict = ", i.infer_dict, " proof_dict = ", i.proof_dict)
-            output_value = graph_in.infer_forward(i.infer_dict, i.proof_dict)
+            if "forward" in method:
+                output_value = graph_in.infer_forward(i.infer_dict, i.proof_dict)
+            elif "likelihood" in method:
+                output_value = graph_in.infer_likelihood(i.infer_dict, i.proof_dict)
             output_file.write(str(output_value) + "\n")
 
         output_file.close()
 
     start_time = time.time()
     # print(graph_in.infer_forward({'I': 'Thap'}, {'D': 'De', 'G': 'B', 'L': 'Yeu', 'S': 'Thap'}))
-    print(graph_in.infer_likelihood({'G': 'A'}, {'L': 'Manh', 'S': 'Thap'}))
+    # print(graph_in.infer_likelihood({'G': 'A'}, {'L': 'Manh', 'S': 'Thap'}))
     print("Time: ", time.time() - start_time)
 
 
